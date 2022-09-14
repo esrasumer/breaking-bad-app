f74a1dcf778eff3e1f973e7ded8dc1bba005775d
esrasumer <esrasumer28@gmail.com>
2022-09-14 17:33:47 +0300

    redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCharacters } from '../redux/charactersSlice'
+import Masonry from 'react-masonry-css'
+import './style.css'
 
 function Home() {
     const characters = useSelector((state) => state.characters.items);
@@ -12,14 +14,18 @@ function Home() {
 
     console.log(characters);
     return (
-        <div>
-            <h1>characters</h1>
-            {characters.map((character) => (
-                <div key={character.char_id}>
-                    <img src={character.img} alt="" />
-                </div>
+        <div className='characters'>
+            <h1 className="title">characters</h1>
+            <div className="character">
+                {characters.map((character) => (
+                    <div key={character.char_id}>
+                        <div>
+                            <img src={character.img} alt="" />
+                        </div>
+                    </div>
 
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -1,10 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios';
 
-export const fetchCharacters = createAsyncThunk('characters/getCharacters', async () => {
-    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
-    return res.data
-})
+export const fetchCharacters = createAsyncThunk('characters/getCharacters',
+    async () => {
+        const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
+        return res.data
+    })
 
 export const charactersSlice = createSlice({
     //store.js'e dahil edicez
@@ -15,7 +16,7 @@ export const charactersSlice = createSlice({
     reducers: {},
     extraReducers: {
         [fetchCharacters.fulfilled]: (state, action) => {
-            state.item = action.payload; // redux'ta gözükmesi için console.log yerine yazılır
+            state.items = action.payload; // redux'ta gözükmesi için console.log yerine yazılır
         }
     }
 });
