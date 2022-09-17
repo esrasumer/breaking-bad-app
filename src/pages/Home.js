f97d6dd8e6d7beabc3fcccd20ad34928712ca92a
esrasumer <esrasumer28@gmail.com>
2022-09-17 23:26:12 +0300

    css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,46 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCharacters } from '../redux/charactersSlice'
-import Masonry from 'react-masonry-css'
 import './style.css'
 
 function Home() {
     const characters = useSelector((state) => state.characters.items);
-    const dispatch = useDispatch();
+    const isLoading = useSelector((state) => state.characters.isLoading);
+    const error = useSelector((state) => state.characters.error)
 
+    const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchCharacters())
     }, [dispatch]);
 
+
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error: {error}</div>
+    }
+
     console.log(characters);
     return (
-        <div className='characters'>
-            <h1 className="title">characters</h1>
-            <div className="character">
-                {characters.map((character) => (
-                    <div key={character.char_id}>
-                        <div>
-                            <img src={character.img} alt="" />
+        <div className='container'>
+            <h1 className="center">CHARACTERS</h1>
+            <div className="cards">
+                <div className='card'>
+                    <div className='card-inner'>
+                        <div className='card-front'>
+                            {characters.map((character) => (
+                                <div key={character.char_id}>
+                                    <div className='character'>
+                                        <img src={character.img} alt="" className='character-front' />
+                                    </div>
+                                </div>
+
+                            ))}
                         </div>
                     </div>
-
-                ))}
+                </div>
             </div>
         </div>
     )
--- src/redux/charactersSlice.js
+++ src/redux/charactersSlice.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchCharacters = createAsyncThunk('characters/getCharacters',
     async () => {
-        const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
+        const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=13`);
         return res.data
     })
 
@@ -12,12 +12,22 @@ export const charactersSlice = createSlice({
     name: 'characters', //dosyanın ismi
     initialState: {
         items: [],
+        isLoading: false,
     },
     reducers: {},
     extraReducers: {
+        [fetchCharacters.pending]: (state, action) => {
+            state.isLoading = true
+        },
         [fetchCharacters.fulfilled]: (state, action) => {
             state.items = action.payload; // redux'ta gözükmesi için console.log yerine yazılır
-        }
+            state.isLoading = false;
+        },
+        [fetchCharacters.rejected]: (state, action) => {
+            state.error = action.error.message; // redux'ta gözükmesi için console.log yerine yazılır
+            state.isLoading = false;
+        },
+
     }
 });
 
